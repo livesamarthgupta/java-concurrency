27be3cce7155e6e12290139b7bed3dd7ec34d401
Samarth Gupta <livesamarthgupta@gmail.com>
2023-06-01 23:54:40 +0530

    deffered-callback-executor example added

--- deferred-callback-executor/src/main/java/com/javaconcurrency/impl/Callback.java
+++ deferred-callback-executor/src/main/java/com/javaconcurrency/impl/Callback.java
@@ -1,11 +1,11 @@
 package com.javaconcurrency.impl;
 
 public class Callback {
-    long executeAt;
-    String message;
+  long executeAt;
+  String message;
 
-    public Callback(long executeAt, String message) {
-        this.executeAt = executeAt;
-        this.message = message;
-    }
+  public Callback(long executeAt, String message) {
+    this.executeAt = System.currentTimeMillis() + (executeAt * 1000);
+    this.message = message;
+  }
 }
